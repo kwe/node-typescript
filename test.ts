1581ab23f00ea3af362f62ffc1a09ebaa8a706af
kwe <kwevans@gmail.com>
2022-02-22 10:15:35 +0000

    some basic tests

--- test.ts
+++ test.ts
@@ -5,6 +5,23 @@ describe('GET /', () => {
   it('should return 200 OK', async () => {
     const res = await request(server).get('/');
     expect(res.status).toEqual(200);
+    expect(res.body).toEqual({ message: 'Hello World' });
+  });
+});
+
+describe('GET /login', () => {
+  it('should return 200 OK', async () => {
+    const res = await request(server).get('/login');
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({ message: 'Login' });
+  });
+});
+
+describe('GET /query', () => {
+  it('should return 200 OK', async () => {
+    const res = await request(server).get('/query');
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({ message: 'Query' });
   });
 });
 
